f462bbdab5294c4238998668f6ba36ef9113224d
Mr ZeDD <65583324+Em710@users.noreply.github.com>
2021-06-21 09:53:57 +0500

    Jutt

--- ZeDD.py
+++ ZeDD.py
@@ -107,7 +107,7 @@ def masuk():
     print ("[2] Login With Access Token")
     print ("[0] Back")
     print
-    msuk = raw_input('⌯⌯⌯⌯⌯⋙')
+    msuk = raw_input("💦💫👉🏼")
     if msuk == '':
         print ("[!] Fill in Correctly")
         masuk()
